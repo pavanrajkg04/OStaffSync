0aebbccf06040e6feb0a63e68a86912434cae6e6
Pavanraj-kine <pavanraj.kg@kine.ai>
2025-10-19 03:17:30 +0530

    j

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="OStaffSync",
+     api_url="https://ostaffsync.onrender.com",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
