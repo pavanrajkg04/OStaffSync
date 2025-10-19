bcd9cc8977b10773bc562172276c29dd80ba9cd5
Pavan Raj K G <83067901+pavanrajkg04@users.noreply.github.com>
2025-10-19 13:16:34 +0530

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="OStaffSync",
-     api_url="https://4c75dac5382d.ngrok-free.app",
+     api_url="https://319eebe35928.ngrok-free.app",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
