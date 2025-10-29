b5846108a05f1b08db66c1555c33a822a125d090
Pavanraj-kine <pavanraj.kg@kine.ai>
2025-10-29 20:17:03 +0530

    new code 29 oct

--- backend/rxconfig.py
+++ backend/rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="OStaffSync",
-     api_url="https://4c75dac5382d.ngrok-free.app",
+     api_url="https://ostaffsync-backend.onrender.com",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
